ef04e3d017a958224c3a2b0362e06ad4d5d17489
amanda vikdal <vikdal.amanda@gmail.com>
2023-03-22 09:57:09 -0500

    cleaned chakra detail tiles

--- src/ChakraDetail.js
+++ src/ChakraDetail.js
@@ -8,14 +8,16 @@ function ChakraDetail({ chakra }) {
     return (
             <div key={chakra.id} className="chakraTile">
                 <img src={chakra.image} alt={chakra.name} />
-                <h2>Sanskrit Name: {chakra.name}</h2>
-                <h2>English Name: {chakra.english_name}</h2>
+                <div>
+                <h4>Sanskrit Name: {chakra.name}</h4>
+                <h4>English Name: {chakra.english_name}</h4>
                 <span>
                  <p>Location in Body: {chakra.location}</p>
                  <p>Supportive Yoga Poses: {chakra.yoga}</p>
                  <p>Element: {chakra.element}</p>
                  <p>Color: {chakra.color}</p>
                 </span>
+                </div>
             </div>
     );
 }
